23b6defdecff1babef7399618e54820d85e5ae95
tiredwren <moudgalya.sneha@gmail.com>
2025-07-25 11:55:54 -0700

    Added Android folder. Getting message "Execution failed for task ':capacitor-android:compileDebugJavaWithJavac'.         > error: invalid source release: 21"

---
+++ android/app/src/main/java/com/duodevelopers/clarity/MainActivity.java
@@ -0,0 +1,5 @@
+package com.duodevelopers.clarity;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.7bfbd50263ff49688f558365875f8497',
-  appName: 'mindful-mobile-zen',
-  webDir: 'dist',
-  server: {
-    url: 'https://7bfbd502-63ff-4968-8f55-8365875f8497.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    UsageStats: {
-      requestPermissions: true
-    }
-  }
+  appId: 'com.duodevelopers.clarity',
+  appName: 'Clarity',
+  webDir: 'dist'
 };
 
 export default config;
